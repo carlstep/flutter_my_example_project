779fe4fc58af99f7f5eabadd04004b768fdebce6
carlstep <carlstep333@me.com>
2022-08-01 23:18:09 +0700

    added TabBar structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MainPage(),
     );
   }
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
 var contentPageIndex = 0;
 
 class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
   @override
   State<MainPage> createState() => _MainPageState();
 }
@@ -49,84 +51,113 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Flutter Project'),
-      ),
-      body: contentPage[contentPageIndex],
-      endDrawer: Drawer(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 300,
-              child: DrawerHeader(
-                child: Column(
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 0,
+      child: Scaffold(
+        backgroundColor: Colors.grey[600],
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Defaults.drawerItemIcon[0],
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Defaults.drawerItemIcon[1],
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Defaults.drawerItemIcon[2],
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Defaults.drawerItemIcon[3],
+                ),
+              ),
+            ],
+          ),
+          title: const Text('My Flutter Project'),
+        ),
+        body: contentPage[contentPageIndex],
+        endDrawer: Drawer(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                child: DrawerHeader(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                      ),
+                      Text(
+                        'My Name',
+                        style: GoogleFonts.poppins(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        'email@gmail.com',
+                        style: GoogleFonts.poppins(
+                            fontSize: 14, fontWeight: FontWeight.w200),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView(
                   children: [
-                    CircleAvatar(
-                      radius: 50,
+                    AppDrawerListTile(
+                      index: 0,
+                      onTap: updateState(0),
                     ),
-                    Text(
-                      'My Name',
-                      style: GoogleFonts.poppins(
-                          fontSize: 20, fontWeight: FontWeight.w600),
+                    AppDrawerListTile(
+                      index: 1,
+                      onTap: updateState(1),
                     ),
-                    Text(
-                      'email@gmail.com',
-                      style: GoogleFonts.poppins(
-                          fontSize: 14, fontWeight: FontWeight.w200),
+                    AppDrawerListTile(
+                      index: 2,
+                      onTap: updateState(2),
+                    ),
+                    AppDrawerListTile(
+                      index: 3,
+                      onTap: updateState(3),
                     ),
                   ],
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView(
-                children: [
-                  AppDrawerListTile(
-                    index: 0,
-                    onTap: updateState(0),
-                  ),
-                  AppDrawerListTile(
-                    index: 1,
-                    onTap: updateState(1),
-                  ),
-                  AppDrawerListTile(
-                    index: 2,
-                    onTap: updateState(2),
-                  ),
-                  AppDrawerListTile(
-                    index: 3,
-                    onTap: updateState(3),
-                  ),
-                ],
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Defaults.drawerItemIcon[0],
               ),
+              label: Defaults.drawerItemText[0],
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Defaults.drawerItemIcon[1],
+              ),
+              label: Defaults.drawerItemText[1],
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Defaults.drawerItemIcon[0],
-            ),
-            label: Defaults.drawerItemText[0],
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Defaults.drawerItemIcon[1],
-            ),
-            label: Defaults.drawerItemText[1],
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          setState(() {});
-        },
-        label: Text('Save'),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            setState(() {});
+          },
+          label: Text('Save'),
+        ),
       ),
     );
   }
